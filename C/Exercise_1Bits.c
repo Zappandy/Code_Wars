54476a6fb980f8e3191f7ffc55e7ee54d21b5780
Andres Gonzalez Gongora <30781086+Zappandy@users.noreply.github.com>
2020-08-26 19:30:12 -0500

    Create Exercise_1Bits.c

---
+++ C/Exercise_1Bits.c
@@ -0,0 +1,31 @@
+#include <stddef.h>  // size_t
+#include <stdlib.h>  // atoi
+size_t countBits(unsigned value);
+
+int main(int argc, const char* argv[])
+{
+        if (argc == 2)
+	{
+		countBits(atoi(argv[1]));		
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+size_t countBits(unsigned value)
+{
+	unsigned bin_dig;
+	long count = 0;
+	while (value > 0)
+	{
+		bin_dig = value % 2;
+		value /= 2;
+		if (bin_dig == 1)
+		{
+			count++;
+		}
+	}
+	return count;
+}
